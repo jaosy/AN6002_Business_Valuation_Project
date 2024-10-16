e92e740bd0539716cc58d2f1a1181a105c65b1e0
Jacqueline Ong <jaosy.29@gmail.com>
2024-10-16 13:30:22 +0800

    Slight improvement

--- bvapp/src/App.js
+++ bvapp/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Plot from "react-plotly.js";
-import { BounceLoader } from "react-spinners"; // Import the spinner
+import { BounceLoader } from "react-spinners";
 import sp500Json from "./sp500_tickers.json";
 import GeoChart from "./map.js";
 
@@ -28,8 +28,8 @@ function App() {
   const [industry, setIndustry] = useState("");
   const [company, setCompany] = useState("");
   const [timePeriod, setTimePeriod] = useState("");
-  const [stockData, setStockData] = useState(null); // State for stock data
-  const [stockValuation, setStockValuation] = useState(null); // State for stock valuation
+  const [stockData, setStockData] = useState(null);
+  const [stockValuation, setStockValuation] = useState(null);
   const [stockDataPlot, setStockDataPlot] = useState(null);
   const [forecastPlot, setForecastPlot] = useState(null);
   const [companyPlot, setCompanyPlot] = useState(null);
@@ -75,7 +75,6 @@ function App() {
       setIndustryPEPlot(JSON.parse(data["industry_plot"]));
       setStockDataPlot(JSON.parse(data["plot"]));
 
-      // news
       const newsResponse = await fetch("/api/news", {
         method: "POST",
         headers: {
@@ -162,7 +161,7 @@ function App() {
           >
             <option value="">Select Company</option>
             {Object.entries(sp500Json)
-              .filter(([ticker, data]) => data["GICS Sector"] === industry) // Filter by industry
+              .filter(([ticker, data]) => data["GICS Sector"] === industry)
               .map(([ticker, data]) => (
                 <option key={ticker} value={ticker}>
                   {data["Security"]}
@@ -190,8 +189,6 @@ function App() {
           </select>
         </div>
         <button
-          variant="contained"
-          color="primary"
           type="submit"
           disabled={timePeriod === "" || industry === "" || company === ""}
           style={
@@ -210,206 +207,151 @@ function App() {
         </div>
       )}
 
-      <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-        {news && (
-          <div
-            style={{
-              maxWidth: "800px",
-              margin: "0 auto",
-              backgroundColor: "#f9f9f9",
-              padding: "20px",
-              borderRadius: "10px",
-              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-            }}
-          >
-            {/* Render average sentiment score and category */}
-            <div
-              style={{
-                padding: "15px",
-                marginBottom: "20px",
-                backgroundColor: "#fff",
-                borderRadius: "8px",
-                boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-              }}
-            >
-              <h2 style={{ margin: "0", color: "#333" }}>
-                News Sentiment Overview
-              </h2>
-              <p style={{ fontSize: "18px", margin: "10px 0" }}>
-                <span style={{ fontWeight: "bold", color: "#546e7a" }}>
-                  Sentiment Score:
-                </span>
-                <span
-                  style={{
-                    fontSize: "20px",
-                    color: "#546e7a",
-                    marginLeft: "10px",
-                  }}
-                >
-                  {news.average_sentiment_score}
-                </span>
-              </p>
-              <p style={{ fontSize: "18px", margin: "10px 0" }}>
-                <span style={{ fontWeight: "bold", color: "#546e7a" }}>
-                  Sentiment Category:
-                </span>
-                <span
-                  style={{
-                    fontSize: "20px",
-                    color: "#546e7a",
-                    marginLeft: "10px",
-                  }}
-                >
-                  {news.avg_sentiment_category}
-                </span>
-              </p>
-            </div>
+      <div style={styles.contentContainer}>
+        <div style={styles.column}>
+          {news && (
+            <div style={styles.newsContainer}>
+              <div style={styles.newsSentimentOverview}>
+                <h2 style={styles.newsHeader}>News Sentiment Overview</h2>
+                <p style={styles.newsSentimentText}>
+                  <span style={styles.newsSentimentLabel}>
+                    Sentiment Score:
+                  </span>
+                  <span style={styles.newsSentimentValue}>
+                    {news.average_sentiment_score}
+                  </span>
+                </p>
+                <p style={styles.newsSentimentText}>
+                  <span style={styles.newsSentimentLabel}>
+                    Sentiment Category:
+                  </span>
+                  <span style={styles.newsSentimentValue}>
+                    {news.avg_sentiment_category}
+                  </span>
+                </p>
+              </div>
 
-            {/* Map through top_news */}
-            <div>
               {Object.keys(news.top_news).map((key) => (
-                <div
-                  key={key}
-                  style={{
-                    padding: "15px",
-                    marginBottom: "20px",
-                    backgroundColor: "#fff",
-                    borderRadius: "8px",
-                    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-                  }}
-                >
-                  <h3
-                    style={{
-                      marginBottom: "10px",
-                      color: "#333",
-                      textAlign: "left",
-                    }}
-                  >
-                    {news.top_news[key].title}
-                  </h3>
-                  <p
-                    style={{
-                      fontSize: "16px",
-                      lineHeight: "1.6",
-                      color: "#555",
-                      textAlign: "left",
-                    }}
-                  >
-                    {news.top_news[key].text}
-                  </p>
+                <div key={key} style={styles.newsItem}>
+                  <h3 style={styles.newsTitle}>{news.top_news[key].title}</h3>
+                  <p style={styles.newsText}>{news.top_news[key].text}</p>
                   <a
                     href={news.top_news[key].news_url}
                     target="_blank"
                     rel="noopener noreferrer"
-                    style={{
-                      display: "inline-block",
-                      marginTop: "10px",
-                      fontSize: "16px",
-                      color: "#007BFF",
-                      textDecoration: "none",
-                    }}
+                    style={styles.newsLink}
                   >
                     Read more &rarr;
                   </a>
                 </div>
               ))}
             </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
 
-      {stockData && (
-        <div style={styles.resultContainer}>
-          <h2 style={styles.resultHeader}>{stockData.company} Overview</h2>
-          <div sx={{ display: "flex", flexDirection: "col" }}>
-            <div style={{ marginBottom: "20px" }}>
-              {timePeriod !== "1 day" && (
-                <>
-                  <Plot
-                    data={stockDataPlot.data}
-                    layout={stockDataPlot.layout}
-                  />
-                  <div>
+        <div style={styles.column}>
+          {stockData && (
+            <div style={styles.resultContainer}>
+              <h2 style={styles.resultHeader}>{stockData.company} Overview</h2>
+              <div style={styles.plotsContainer}>
+                {timePeriod !== "1 day" && (
+                  <>
+                    <Plot
+                      data={stockDataPlot.data}
+                      layout={stockDataPlot.layout}
+                      style={styles.plot}
+                    />
                     <Plot
                       data={forecastPlot.data}
                       layout={forecastPlot.layout}
+                      style={styles.plot}
                     />
-                  </div>
-                </>
-              )}
-            </div>
-            <div>
-              <Plot data={industryPEPlot.data} layout={industryPEPlot.layout} />
-            </div>
-            <div>
-              <Plot data={companyPlot.data} layout={companyPlot.layout} />
-            </div>
-          </div>
-          <div style={styles.infoContainer}>
-            <h3 style={styles.infoHeader}>Summary Data</h3>
-            <ul style={styles.list}>
-              {Object.entries(stockData.summary_data).map(([key, value]) => (
-                <li key={key} style={styles.listItem}>
-                  {key}: {value}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div style={styles.infoContainer}>
-            <h3 style={styles.infoHeader}>Company Info</h3>
-            {stockData["info"]["Company Logo"] && (
-              <div style={{ marginBottom: "10px" }}>
-                <img
-                  src={stockData["info"]["Company Logo"]}
-                  alt="Company Logo"
-                  style={{ width: "200px", height: "200px" }} // Adjust size as needed
+                  </>
+                )}
+                <Plot
+                  data={industryPEPlot.data}
+                  layout={industryPEPlot.layout}
+                  style={styles.plot}
+                />
+                <Plot
+                  data={companyPlot.data}
+                  layout={companyPlot.layout}
+                  style={styles.plot}
                 />
               </div>
-            )}
-            <ul style={styles.list}>
-              {orderedKeys.map(
-                (key) =>
-                  stockData.info[key] !== undefined && (
-                    <li key={key} style={styles.listItem}>
-                      {key}: {stockData.info[key]}
-                    </li>
-                  )
-              )}
-            </ul>
-            <GeoChart
-              sx={{ marginBottom: 0 }}
-              state={
-                stockData.info["Address"]
-                  .split(",")[2]
-                  .trim()
-                  .split(" ")
-                  .slice(-2, -1)[0]
-              }
-            />
-          </div>
+              <div style={styles.infoContainer}>
+                <h3 style={styles.infoHeader}>Summary Data</h3>
+                <ul style={styles.list}>
+                  {Object.entries(stockData.summary_data).map(
+                    ([key, value]) => (
+                      <li key={key} style={styles.listItem}>
+                        {key}: {value}
+                      </li>
+                    )
+                  )}
+                </ul>
+              </div>
+              <div style={styles.infoContainer}>
+                <h3 style={styles.infoHeader}>Company Info</h3>
+                {stockData["info"]["Company Logo"] && (
+                  <div style={styles.logoContainer}>
+                    <img
+                      src={stockData["info"]["Company Logo"]}
+                      alt="Company Logo"
+                      style={styles.logo}
+                    />
+                  </div>
+                )}
+                <ul style={styles.list}>
+                  {orderedKeys.map(
+                    (key) =>
+                      stockData.info[key] !== undefined && (
+                        <li key={key} style={styles.listItem}>
+                          {key}: {stockData.info[key]}
+                        </li>
+                      )
+                  )}
+                </ul>
+                <GeoChart
+                  state={
+                    stockData.info["Address"]
+                      .split(",")[2]
+                      .trim()
+                      .split(" ")
+                      .slice(-2, -1)[0]
+                  }
+                />
+              </div>
 
-          {stockValuation && (
-            <div style={styles.infoContainer}>
-              <h3 style={styles.infoHeader}>Valuation Details</h3>
-              <ul style={styles.list}>
-                <li>Company Name: {stockValuation["Company Name"]}</li>
-                <li>Sector: {stockValuation.Sector}</li>
-                <li>
-                  Enterprise Value (Millions):{" "}
-                  {stockValuation["Enterprise Value (Millions)"]}
-                </li>
-                <li>
-                  Net Debt (Millions): {stockValuation["Net Debt (Millions)"]}
-                </li>
-                <li>
-                  Equity Value (Millions):{" "}
-                  {stockValuation["Equity Value (Millions)"]}
-                </li>
-              </ul>
+              {stockValuation && (
+                <div style={styles.infoContainer}>
+                  <h3 style={styles.infoHeader}>Valuation Details</h3>
+                  <ul style={styles.list}>
+                    <li style={styles.listItem}>
+                      Company Name: {stockValuation["Company Name"]}
+                    </li>
+                    <li style={styles.listItem}>
+                      Sector: {stockValuation.Sector}
+                    </li>
+                    <li style={styles.listItem}>
+                      Enterprise Value (Millions):{" "}
+                      {stockValuation["Enterprise Value (Millions)"]}
+                    </li>
+                    <li style={styles.listItem}>
+                      Net Debt (Millions):{" "}
+                      {stockValuation["Net Debt (Millions)"]}
+                    </li>
+                    <li style={styles.listItem}>
+                      Equity Value (Millions):{" "}
+                      {stockValuation["Equity Value (Millions)"]}
+                    </li>
+                  </ul>
+                </div>
+              )}
             </div>
           )}
         </div>
-      )}
+      </div>
     </div>
   );
 }
@@ -417,26 +359,25 @@ function App() {
 const styles = {
   app: {
     fontFamily: "Arial, sans-serif",
-    maxWidth: "800px",
     margin: "0 auto",
     padding: "20px",
-    textAlign: "center",
-    backgroundColor: "#f9f9f9",
-    borderRadius: "8px",
-    boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
+    maxWidth: "100%",
   },
   header: {
     color: "#333",
     marginBottom: "20px",
+    textAlign: "center",
   },
   form: {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+    marginBottom: "20px",
   },
   formGroup: {
     marginBottom: "15px",
     width: "100%",
+    maxWidth: "400px",
   },
   label: {
     display: "block",
@@ -472,37 +413,92 @@ const styles = {
     backgroundColor: "#ccc",
     cursor: "not-allowed",
   },
-  resultContainer: {
-    position: "relative",
-    marginTop: "20px",
-    textAlign: "left",
-    padding: "20px",
-    backgroundColor: "#fff",
+  contentContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+  },
+  column: {
+    flex: "0 0 48%",
+    marginBottom: "20px",
+  },
+  newsContainer: {
+    marginBottom: "20px",
+  },
+  newsSentimentOverview: {
+    backgroundColor: "#f0f8ff",
+    padding: "15px",
     borderRadius: "8px",
-    boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
+    marginBottom: "20px",
+  },
+  newsHeader: {
+    fontSize: "20px",
+    marginBottom: "10px",
+    color: "#333",
+  },
+  newsSentimentText: {
+    fontSize: "16px",
+    margin: "5px 0",
+  },
+  newsSentimentLabel: {
+    fontWeight: "bold",
+    marginRight: "10px",
+  },
+  newsSentimentValue: {
+    color: "#007bff",
+  },
+  newsItem: {
+    backgroundColor: "#fff",
+    padding: "15px",
+    borderRadius: "8px",
+    marginBottom: "15px",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+  },
+  newsTitle: {
+    fontSize: "18px",
+    marginBottom: "10px",
+    color: "#333",
+  },
+  newsText: {
+    fontSize: "14px",
+    lineHeight: "1.6",
+    color: "#555",
+  },
+  newsLink: {
+    display: "inline-block",
+    marginTop: "10px",
+    color: "#007bff",
+    textDecoration: "none",
+  },
+  resultContainer: {
+    marginBottom: "20px",
   },
   resultHeader: {
     marginBottom: "10px",
     fontSize: "20px",
     fontWeight: "bold",
   },
-  plotContainer: {
+  plotsContainer: {
     display: "flex",
-    justifyContent: "center",
-    marginBottom: "15px",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  plot: {
+    width: "100%",
+    marginBottom: "20px",
   },
   infoContainer: {
     marginTop: "15px",
     padding: "15px",
     borderRadius: "8px",
-    backgroundColor: "#f0f8ff", // Light blue background for info sections
+    backgroundColor: "#f0f8ff",
     boxShadow: "0 1px 3px rgba(0, 0, 0, 0.1)",
   },
   infoHeader: {
     marginBottom: "10px",
     fontSize: "18px",
     fontWeight: "bold",
-    color: "#007bff", // Bootstrap primary color
+    color: "#007bff",
   },
   list: {
     listStyleType: "none",
